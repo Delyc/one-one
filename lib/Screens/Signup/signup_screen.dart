9bb2e3dca9f70d12729e5b84d4c034d0914c7174
Delyc <d.twizeyima@alustudent.com>
2023-02-14 17:04:45 +0200

    Feat: Implementing post page

--- lib/Screens/Signup/signup_screen.dart
+++ lib/Screens/Signup/signup_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_auth/constants.dart';
 import 'package:flutter_auth/responsive.dart';
 import '../../components/background.dart';
-import 'components/sign_up_top_image.dart';
 import 'components/signup_form.dart';
 import 'components/socal_sign_up.dart';
 
@@ -17,9 +16,7 @@ class SignUpScreen extends StatelessWidget {
           mobile: const MobileSignupScreen(),
           desktop: Row(
             children: [
-              const Expanded(
-                child: SignUpScreenTopImage(),
-              ),
+            
               Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -51,7 +48,6 @@ class MobileSignupScreen extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        const SignUpScreenTopImage(),
         Row(
           children: const [
             Spacer(),
